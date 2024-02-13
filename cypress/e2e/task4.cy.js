ee5fb534d4aca9d333b95e6903807a44c15c9aa5
Rula2022 <rulaammar13@gmail.com>
2024-02-13 14:37:38 +0200

    add selector file

--- cypress/e2e/task4.cy.js
+++ cypress/e2e/task4.cy.js
@@ -1,12 +1,35 @@
-/// <reference types="cypress"/>  
-
-it("Add item to cart",()=>{
-  cy.visit("/")
-  cy.get("#search").type("shirt"+"{enter}",{delay:500});
-  cy.get("div.product-item-info").eq(0).click({delay:500})
-  cy.get(".swatch-option.text").eq(2).click()
-  cy.get(".swatch-option.color").eq(2).click({delay:500})
-  cy.get("#qty").clear().type("3")
-  cy.contains("Add to Cart").click()
-  }
-  )
+/// <reference types="cypress" />
+it('task',()=>{
+  cy.visit("https://demo.productionready.io/#/");
+  cy.contains("a","conduit");
+  cy.get(".navbar a+ul li:first-child");
+  cy.get(".navbar a+ul li:last-child");
+  cy.get(".banner .logo-font");
+  cy.get(".banner .logo-font + p");
+  cy.get(".feed-toggle li:nth-child(2) a");
+  cy.contains("span","Read more...");
+  cy.get(".btn-outline-primary").first();
+  cy.get(".sidebar p");
+  cy.get(".sidebar .tag-list a").eq(3);
+  cy.get(".sidebar .tag-list a").last();
+  // ------ 
+  // you can visit the page by the link
+  cy.get(".preview-link").first().click();
+  cy.wait(1500);
+  cy.get(".banner h1");
+  cy.get(".banner .article-meta img");
+  cy.get(".banner .info a");
+  cy.get(".banner .info span");
+  cy.get(".banner follow-btn");
+  cy.get(".banner favorite-btn");
+  cy.get(".article-content p");
+  cy.get(".tag-list li:first-child");
+  cy.get(".tag-list li").last();
+  // ------ 
+  cy.visit("https://demo.productionready.io/#/login");
+  cy.contains("h1","Sign in");
+  cy.get(".auth-page p  a:last-child");
+  cy.get("input[type=email]");
+  cy.get("input[type=password]");
+  cy.get("[type=submit]");
+});
